790aa33fcaa03b6991586ddd67280367da70ec25
John Tan <john.v.tan@gmail.com>
2022-02-19 19:55:55 -0500

    Add optimized interpreter

---
+++ ch4/optimized-interpreter/eval.rkt
@@ -0,0 +1,124 @@
+#!/usr/bin/racket
+
+#lang sicp 
+
+(#%require "syntax.rkt")
+(#%require "environment.rkt")
+
+; Evaluates each element in exps
+; This is how applicative order is implemented
+; Before each call to myapply, list-of-values is called
+; which myevaluates each of the operands in the arglist
+; before the procedure is applied.
+; (define (list-of-values exps env)
+;   (if (no-operands? exps)
+;     '()
+;     (cons (myeval (first-operand exps) env)
+;           (list-of-values (rest-operands exps) env))))
+
+(define (analyze-self-evaluating exp)
+  (lambda (env) exp))
+
+(define (analyze-quoted exp)
+  (let [(qval (text-of-quotation exp))]
+    (lambda (env) qval)))
+
+; has to be done at execution time
+(define (analyze-variable exp)
+  (lambda (env) (lookup-variable-value exp env)))
+
+(define (analyze-assignment exp)
+  (let [(var (assignment-variable exp))
+        ; value is constructed (maybe?) at analysis time
+        ; unless this is another variable to be looked up at exec time
+        (vproc (analyze (assignment-value exp)))]
+    (lambda (env)
+      (set-variable-value! var (vproc env) env)
+      'ok)))
+
+(define (analyze-definition exp)
+  (let [(var (definition-variable exp))
+        (vproc (analyze (definition-value exp)))]
+    (lambda (env)
+      (define-variable! var (vproc env) env)
+      'ok)))
+
+(define (analyze-if exp)
+  (let [(pproc (analyze (if-predicate exp)))
+        (cproc (analyze (if-consequent exp)))
+        (aproc (analyze (if-alternative exp)))]
+    (lambda (env) (if (pproc env) (cproc env) (aproc env)))))
+
+(define (analyze-lambda exp)
+  (let [(vars (lambda-parameters exp))
+        (bproc (analyze-sequence (lambda-body exp)))]
+    (lambda (env) (make-procedure vars bproc env))))
+
+(define (analyze-sequence exps)
+  (define (sequentially proc1 proc2)
+    (lambda (env) (proc1 env) (proc2 env)))
+  
+  (define (loop first-proc rest-procs)
+    (if (null? rest-procs)
+      first-proc
+      (loop (sequentially first-proc (car rest-procs))
+            (cdr rest-procs))))
+  (let ([procs (map analyze exps)])
+    (if (null? procs) (error "Empty sequence: ANALYZE")
+        (loop (car procs) (cdr procs)))))
+
+(define (analyze-application exp)
+  (let [(fproc (analyze (operator exp)))
+        (aprocs (map analyze (operands exp)))]
+    (lambda (env)
+      (execute-application
+        (fproc env)
+        (map (lambda (aproc) (aproc env)) aprocs)))))
+
+(define (execute-application proc args)
+  (cond
+    [(primitive-procedure? proc)
+      (myapply-primitive-procedure proc args)]
+    [(compound-procedure? proc)
+      ((procedure-body proc)
+        (extend-environment (procedure-parameters proc)
+          args
+          (procedure-environment proc)))]
+    [else
+      (error "Unknown procedure type: EXECUTE-APPLICATION" proc)]))
+
+(define (analyze exp)
+  (cond
+    [(self-evaluating? exp) (analyze-self-evaluating exp)]
+    [(quoted? exp) (analyze-quoted exp)]
+    [(variable? exp) (analyze-variable exp)]
+    [(assignment? exp) (analyze-assignment exp)]
+    [(definition? exp) (analyze-definition exp)]
+    [(if? exp) (analyze-if exp)]
+    [(lambda? exp) (analyze-lambda exp)]
+    [(begin? exp) (analyze-sequence (begin-actions exp))]
+    [(cond? exp) (analyze (cond->if exp))]
+    [(application? exp) (analyze-application exp)]
+    [else (error "Unknown expression type: ANALYZE" exp)]))
+
+(define (myeval exp env)
+  ((analyze exp) env))
+
+(define (myapply-primitive-procedure proc args)
+  (apply (primitive-implementation proc) args))
+;
+;(define (myapply procedure arguments)
+;  (cond
+;    [(primitive-procedure? procedure)
+;      (myapply-primitive-procedure procedure arguments)]
+;    [(compound-procedure? procedure)
+;      (myeval-sequence
+;        (procedure-body procedure)
+;        (extend-environment
+;          (procedure-parameters procedure)
+;          arguments
+;          (procedure-environment procedure)))]
+;    [else
+;      (error "Unknown procedure type: APPLY" procedure)]))
+;
+(#%provide myeval)
---
+++ ch4/optimized-interpreter/test.rkt
@@ -0,0 +1,123 @@
+#!/usr/bin/racket
+
+#lang sicp 
+
+(#%require "eval.rkt")
+(#%require "environment.rkt")
+(#%require "assert.rkt")
+
+(define (eval-with-env expr)
+  (myeval expr (setup-environment)))
+
+(assert-eq (eval-with-env '3) 3)
+(assert-eq (eval-with-env '(+ 1 2)) 3)
+(assert-eq (eval-with-env '(* 4 3)) 12)
+(assert-eq (eval-with-env '(cons 1 2)) (cons 1 2))
+
+; if statements
+(assert-eq (eval-with-env '(if (eq? 3 4) 0 1)) 1)
+(assert-eq (eval-with-env '(if (eq? 4 4) 0 1)) 0)
+(assert-eq (eval-with-env '(if (if (eq? 3 4) true false) (+ 3 8) (* 4 2))) 8)
+(assert-eq 
+  (eval-with-env
+    '((if (eq? 10 11) + -) 3 3))
+  0)
+
+; conds
+(assert-eq
+  (eval-with-env
+    '(cond
+       [(eq? 3 4) "no"]
+       [(eq? 8 6) "no"]
+       [(eq? (+ 3 3) 6) "yes"]
+       [else "no"]))
+  "yes")
+
+(assert-eq
+  (eval-with-env
+    '(cond
+       [(eq? 3 4) "no"]
+       [(eq? 8 6) "no"]
+       [(eq? (+ 3 3) 7) "no"]
+       [else "yes"]))
+  "yes")
+
+; define + begin 
+(assert-eq
+  (eval-with-env
+    '(begin
+      (define x 3)
+      (define y 8)
+      (* x y)))
+  24)
+
+; lambda
+(assert-eq
+  (eval-with-env
+    '((lambda (x) (+ x 2)) 5))
+  7)
+
+; assignment
+(assert-eq
+  (eval-with-env
+    '(begin
+      (define x 4)
+      (set! x 23)
+      (* x 2)))
+  46)
+
+; lambda with different scope 
+(assert-eq
+  (eval-with-env
+    '(begin
+      (define x 23)
+      (define y 42)
+      (define (proc x)
+        (+ x y))
+      (proc 100)))
+  142)
+
+; fact 
+(assert-eq
+  (eval-with-env
+    '(begin
+      (define (fact n)
+        (if (eq? n 0)
+          1
+          (* n (fact (- n 1)))))
+        (fact 5)))
+  120)
+
+; let's
+;(assert-eq
+;  (eval-with-env
+;    '(let [(x 3) (y 4)]
+;      (+ x y)))
+;  7)
+;
+;(assert-eq
+;  (eval-with-env
+;    '(begin
+;      (define outer 42)
+;      (define x 32)
+;      (let [(x 1000)]
+;        (let [(y 23)]
+;          (+ outer x y)))))
+;  (+ 1000 23 42))
+
+(assert-eq
+  (eval-with-env
+    '((lambda ()
+        (define x 3)
+        (+ x 45))))
+  48)
+
+
+(display "TESTS PASS") (newline)
+
+; I don't feel like figuring out how to write a macro that catches exceptions
+; so just uncomment these when you want to test
+
+; (eval-with-env '(begin
+;                   (define x 4)
+;                   (+ y 3)))
